113286e292ccf40f327601b43dbb1442061a5102
Morten Kleveland <mortengkleveland@gmail.com>
2015-06-10 13:03:55 +0200

    We have connection!

--- script.js
+++ script.js
@@ -1,32 +1,20 @@
+
 function moduleDidLoad() {
     setDefaultValues();
     csound.Play();
-    csound.CompileOrc(
-        "instr 1 \n" + 
-        "Sfile = \"./local/soundfile\"\n" +
-        //"icps = 440 + rnd(440) \n" + 
-        //"kPitch chnget \"pitch\" \n" +
-        //"chnset icps, \"freq\" \n" + 
-        "a1 diskin2 Sfile, 1, 0, 0\n" +
-        //"a1 oscili 0.1, kcps\n" + 
-        //"a2 diskin2 \"01.wav\", 1\n" +
-        "outs a1, a1 \n" + 
-        "endin");
+    csound.CompileOrc(document.getElementById('orchestraField').value);
 }
 
 function attachListeners() {
-    document.getElementById("mess").addEventListener("click", Play);
+    //document.getElementById("mess").addEventListener("click", play);
     document.getElementById("openFileButton").addEventListener("change", handleFileSelect);
+    document.getElementById("playButton").addEventListener("click", play);
 }
 
 function handleMessage(message) {
     var mess = message.data;
     if (mess.slice(0, 11) == "::control::") {
-        var messField = document.getElementById("console")
-        messField.innerText = mess.slice(11);
     } else {
-        var messField = document.getElementById("mess")
-        messField.innerText += mess;
         csound.RequestChannel("freq");
     }
 }
@@ -36,8 +24,8 @@ function setDefaultValues() {
     csound.SetChannel("freq", initValue);
 }
 
-function Play() {
-    csound.Event("i1 0 5");
+function play() {
+   csound.Event("i1 0 -1");
 }
 
 function handleFileSelect(evt) {
