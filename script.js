c8a55937c977296c8a415f4918066f0ff44d9f6e
Morten Kleveland <mortengkleveland@gmail.com>
2015-06-10 12:10:54 +0200

    Add audio file import (mono)

--- script.js
+++ script.js
@@ -2,22 +2,26 @@ function moduleDidLoad() {
     setDefaultValues();
     csound.Play();
     csound.CompileOrc(
-    "instr 1 \n" +
-    "icps = 440 \n" +
-    "chnset icps, \"freq\" \n" +
-    "a1 oscili 0.1, icps\n" +
-    "outs a1, a1 \n" +
-    "endin");   
-    document.getElementById("tit").innerHTML = "Click on the page below to play";
+        "instr 1 \n" + 
+        "Sfile = \"./local/soundfile\"\n" +
+        //"icps = 440 + rnd(440) \n" + 
+        //"kPitch chnget \"pitch\" \n" +
+        //"chnset icps, \"freq\" \n" + 
+        "a1 diskin2 Sfile, 1, 0, 0\n" +
+        //"a1 oscili 0.1, kcps\n" + 
+        //"a2 diskin2 \"01.wav\", 1\n" +
+        "outs a1, a1 \n" + 
+        "endin");
 }
 
 function attachListeners() {
     document.getElementById("mess").addEventListener("click", Play);
+    document.getElementById("openFileButton").addEventListener("change", handleFileSelect);
 }
 
 function handleMessage(message) {
     var mess = message.data;
-    if(mess.slice(0, 11) == "::control::") {
+    if (mess.slice(0, 11) == "::control::") {
         var messField = document.getElementById("console")
         messField.innerText = mess.slice(11);
     } else {
@@ -29,10 +33,22 @@ function handleMessage(message) {
 
 function setDefaultValues() {
     var initValue = 0.0;
-    csound.RequestChannel("lok");
     csound.SetChannel("freq", initValue);
 }
 
 function Play() {
     csound.Event("i1 0 5");
+}
+
+function handleFileSelect(evt) {
+    var files = evt.target.files; 
+    var f = files[0];
+    var objectURL = window.URL.createObjectURL(f);
+    playing = false;
+    if(playing) {
+        csound.Event("i-1 0 1");
+        playing = false;   
+    }
+    csound.CopyUrlToLocal(objectURL, "soundfile");
+    selected = true;
 }
